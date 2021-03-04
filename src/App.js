1b4cd00d7b60c8b42eec587dfe9816f2fcf0c655
Giovanni Aranda <giovanni.arandaa@gmail.com>
2021-03-04 08:20:48 -0600

    Creando la interface con styled components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
+import { GlobalStyle } from './styles/GlobalStyle'
+
+export const App = () => (
+  <>
+    <Logo />
+    <GlobalStyle />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </>
+)
