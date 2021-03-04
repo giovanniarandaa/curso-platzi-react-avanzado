6d46fd7edf45232a90c1a6872208dce87f450a27
Giovanni Aranda <giovanni.arandaa@gmail.com>
2021-03-03 23:50:23 -0600

    Preparando entorno de desarrollo

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>Seguimos aqui</h1>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
